bdcc3a445b17f04e5dff3b9d2fa89e169fd98c42
Buzowicz, Patryk <patryk.buzowicz.ctr@sabre.com>
2018-08-01 21:55:42 +0200

    added extra cases for double negation .Not().Not()

--- RoxAssertion.Tests/ExtraNotTests.cs
+++ RoxAssertion.Tests/ExtraNotTests.cs
@@ -35,5 +35,31 @@ namespace RoxAssertion.Tests
         {
             (-10).Expect().Not().IsGreater(4);
         }
+
+        [TestMethod]
+        public void Test_NotNot_Eq()
+        {
+            10.Expect().Not().Not().Eq(10);
+        }
+
+        [TestMethod]
+        public void Test_NotNot_Greater()
+        {
+            10.Expect().Not().Not().IsGreater(5);
+        }
+
+        [TestMethod]
+        public void Test_NotNot_Properties()
+        {
+            var tested = new FizBar() { Bar = "Bar", Fiz = String.Empty };
+            var expected = new { Bar = "Bar", Fiz = String.Empty };
+            tested.Expect().Not().Not().Properties().Eq(expected);
+        }
+
+        [TestMethod]
+        public void Test_NotNot_RaiseError()
+        {
+            (new Action(() => throw new Exception())).Expect().Not().Not().RaiseError();
+        }
     }
 }
